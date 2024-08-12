da768427b03a750f0301db1c530810b88d33d9a2
Cletus-05 <cletusfernandes0520@gmail.com>
2024-08-12 23:43:21 +0530

    updated

--- Halo/src/App.jsx
+++ Halo/src/App.jsx
@@ -1,8 +1,11 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './Components/Navbar/Navbar'
 
 function App() {
-  
+  <div>
+    <Navbar/>
+  </div>
 
   
 }
---
+++ Halo/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Navbar.css'
+import logo from '../Assests/Frontend_Assets/logo.png'
+import cart_icon from '../Assests/cart_icon.png'
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      
+    </div>
+  )
+}
+
+export default Navbar
