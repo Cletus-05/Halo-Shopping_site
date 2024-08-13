e24885c04f0897ee421745b61914bec23d05ab67
Cletus-05 <cletusfernandes0520@gmail.com>
2024-08-14 01:17:46 +0530

    updated

--- Halo/src/Components/Navbar/Navbar.jsx
+++ Halo/src/Components/Navbar/Navbar.jsx
@@ -7,11 +7,19 @@ const Navbar = () => {
   return (
     <div className='navbar'>
       <div className="nav-logo">
-      <img src={logo} alt="halooo" />
-      <p style={{color:"black"}}>SHOPPER</p>
-
+      <img src={logo} alt=""/>
+      <p>SHOPPER</p>
       </div>
-      
+      <ul className="nav-menu">
+        <li>Shop</li>
+        <li>Men</li>
+        <li>Women</li>
+        <li>Kids</li>
+      </ul>
+      <div className="nav-login-cart">
+        <button>Login</button>
+        <img src={cart_icon} alt="" />
+      </div>
     </div>
   )
 }
